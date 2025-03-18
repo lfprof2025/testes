c64d8ac4f72edd4bd506c17a1c1c879e7eee9a2c
luizhanke25 <luiz@hankeds.com.br>
2025-03-18 16:11:34 -0300

    primeiro commit

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)  # Tempo de espera entre requisições (segundos)
+
+    @task
+    def load_homepage(self):
+        self.client.get("/")  # Testando a página inicial
+
+    @task
+    def load_about_page(self):
+        self.client.get("/presencial/administracao-bacharelado")   
